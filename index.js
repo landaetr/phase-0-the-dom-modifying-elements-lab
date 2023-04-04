4681a3b81597c1554104462850cd92ae8fda8961
richard <richard.landaeta@gmail.com>
2023-04-04 10:09:04 -0500

    all passed test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//const newHeader = document.createElement("h1");
+//newHeader.id = "victory";
+//newHeader.textContent = "Richard is the champion";
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Richard is the champion";
+
+document.body.appendChild(newHeader);
